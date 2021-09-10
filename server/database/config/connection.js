b07b290b789a6bdd328e50e428c0ce102e25ff1e
EmanAmassi <eman.amassi98@gmail.com>
2021-09-10 04:52:09 +0300

    edit db url

--- server/database/config/connection.js
+++ server/database/config/connection.js
@@ -2,7 +2,7 @@ const {Pool} = require('pg');
 
 const dbUrl_dev= process.env.DB_URL_DEV;
 const dbUrl_test= process.env.DB_URL_TEST;
-const dbUrl_prod= process.env.DB_URL;
+const dbUrl_prod= process.env.DATABASE_URL;
 
 const NODE_ENV = process.env.NODE_ENV;
 
